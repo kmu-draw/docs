69cb186f20b84acd63849140091a86abdda17929
Jaewook Ahn <dev.jaewook@gmail.com>
2023-05-19 21:51:30 +0900

    chore: Add ts support
    - add typescript modules
    - add ts files
    - rename theme.config extension to tsx

--- theme.config.jsx
+++
@@ -1,6 +0,0 @@
-export default {
-  logo: <span>DRAW : Diagram Read And Write</span>,
-  project: {
-    link: 'https://github.com/kmu-draw'
-  }
-};
---
+++ theme.config.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import type { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>DRAW : Diagram Read And Write</span>,
+  project: {
+    link: 'https://github.com/kmu-draw'
+  }
+};
+
+export default config;
